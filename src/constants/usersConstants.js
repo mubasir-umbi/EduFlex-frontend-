31af3ce6a9919c9d355276fb2811d3885414b2c6
mubasir umbi <mubasirumbi@gmail.com>
2023-08-24 10:24:15 +0530

    changes made

--- src/constants/usersConstants.js
+++ src/constants/usersConstants.js
@@ -1,4 +1,4 @@
-// export const END_POINT = 'https://eduflex.site'
-export const END_POINT = 'http://localhost:4001'
+export const END_POINT = 'https://eduflex.site'
+// export const END_POINT = 'http://localhost:4001'
 export const USERS_URL = `${END_POINT}/api/users/`
 export const CHAT_URL = `${END_POINT}/api/chat/`
--- src/slices/apiSlice.js
+++ src/slices/apiSlice.js
@@ -1,6 +1,6 @@
 import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react';
-// https://eduflex.site
-const baseQuery = fetchBaseQuery({ baseUrl: '' });
+
+const baseQuery = fetchBaseQuery({ baseUrl: 'https://eduflex.site' });
 
 export const apiSlice = createApi({
   baseQuery,
