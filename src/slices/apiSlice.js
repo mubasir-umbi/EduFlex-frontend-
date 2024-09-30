bfa65ee271565b40536038a70041e1214d2cb8bc
mubasir umbi <mubasirumbi@gmail.com>
2024-09-30 17:55:21 +0530

    updated

--- src/slices/apiSlice.js
+++ src/slices/apiSlice.js
@@ -1,6 +1,7 @@
 import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react';
 
-const baseQuery = fetchBaseQuery({ baseUrl: 'https://eduflex.site' });
+// const baseQuery = fetchBaseQuery({ baseUrl: 'https://eduflex.site' });
+const baseQuery = fetchBaseQuery({ baseUrl: 'https://eduflex-backend-h43b.onrender.com' });
 
 export const apiSlice = createApi({
   baseQuery,
